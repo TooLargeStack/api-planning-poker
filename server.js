c5a7844728ef543f2759c994d404b9b9daf858b3
Leonardo Rodrigues <leonardo.rodrigues@agileprocess.com.br>
2022-04-23 02:05:10 -0300

    Adjusts requirements

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const cors = require('cors')
+const express = require('express');
 
 const sessionController = require('./src/controllers/SessionController')
-const express = require('express');
 
 const app = express();
 app.use(cors({credentials: true, origin: '*'}));
