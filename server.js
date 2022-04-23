c74e846ce60792fab51ce3ce9298c2a949467691
Leonardo Rodrigues <leonardo.rodrigues@agileprocess.com.br>
2022-04-23 02:04:15 -0300

    Chore: fixes socket.io version and fixes cors errors

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
+const cors = require('cors')
+
 const sessionController = require('./src/controllers/SessionController')
 const express = require('express');
 
 const app = express();
-const server = require('http').createServer(app);
-const port = 3000;
+app.use(cors({credentials: true, origin: '*'}));
 
-const session = new sessionController(server)
-session.connect()
+const server = require('http').createServer(app);
+const port = 5000;
+
+const session = new sessionController()
+session.connect(server)
+
+app.all('/', function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 app.use('/', (req, res) => {
-    res.send('hello world');
+    res.send(`aaaa`)
+    console.log('testing')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
 
